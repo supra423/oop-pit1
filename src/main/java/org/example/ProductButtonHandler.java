81908ce9e950f66376ed7e53def791516a754d8c
supra <alexusenzoroa@gmail.com>
2025-09-22 19:13:21 +0800

    Started making use of the orders ArrayList and then able to ask the quantity of a product and then multiplying it to the price of the product.

--- src/main/java/org/example/ProductButtonHandler.java
+++ src/main/java/org/example/ProductButtonHandler.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.gui.PaymentPanel;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,7 +10,13 @@ public record ProductButtonHandler(Product product) implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        String messageAppend1 = String.format("Product Name: %s -- Cost: Php%.2f\n", product.getName(), product.getPrice());
+        BoughtItem boughtItem = new BoughtItem(product);
+        boughtItem.setQuantity(Integer.parseInt(JOptionPane.showInputDialog("Input quantity of product: ")));
+        Order.appendOrders(boughtItem);
+        double priceTimesQuantity = (boughtItem.getProduct().getPrice() * boughtItem.getQuantity());
+        String messageAppend1 = String.format(
+                "%s x %d ----- %.2f\n", boughtItem.getProduct().getName(), boughtItem.getQuantity(), priceTimesQuantity
+        );
         PaymentPanel.getOrderTextArea().append(messageAppend1);
     }
 }
