e8833191c7beb0b393559de6c48bc73f5f554320
supra <alexusenzoroa@gmail.com>
2025-09-23 11:19:04 +0800

    I decided to just place the major components inside the MainPanel constructor, I'm planning on using a different layout manager inside the mainPanel.

--- src/main/java/org/example/gui/MainPanel.java
+++ src/main/java/org/example/gui/MainPanel.java
@@ -1,4 +1,25 @@
 package org.example.gui;
 
+import java.awt.*;
+
 public class MainPanel extends SuperPanel {
+    public MainPanel() {
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        TextAreaPanel paymentPanel = new TextAreaPanel();
+        this.add(paymentPanel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        ProductPanel productPanel = new ProductPanel();
+        this.add(productPanel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        BottomPanel bottomPanel = new BottomPanel();
+        this.add(bottomPanel, gbc);
+    }
 }
--- src/main/java/org/example/gui/Window.java
+++ src/main/java/org/example/gui/Window.java
@@ -19,25 +19,6 @@ public class Window extends JFrame {
         this.setMinimumSize(new Dimension(1302, 1029));
 
         MainPanel mainPanel = new MainPanel();
-        mainPanel.setLayout(new GridBagLayout());
-
-
-        GridBagConstraints gbc = new GridBagConstraints();
-
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        TextAreaPanel paymentPanel = new TextAreaPanel();
-        mainPanel.add(paymentPanel, gbc);
-
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        ProductPanel productPanel = new ProductPanel();
-        mainPanel.add(productPanel, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        BottomPanel bottomPanel = new BottomPanel();
-        mainPanel.add(bottomPanel, gbc);
 
         this.add(mainPanel);
 
