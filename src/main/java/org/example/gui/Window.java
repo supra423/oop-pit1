3139cd71cd57cfc17fa1948c97db3fafba8c5b31
supra <alexusenzoroa@gmail.com>
2025-10-04 01:45:21 +0800

    Made the middle and bottom panels more responsive
    I allowed these panels to stretch and also changed the scrollbar
    of the main panel to only appear when needed

--- src/main/java/org/example/gui/Window.java
+++ src/main/java/org/example/gui/Window.java
@@ -17,11 +17,9 @@ public class Window extends JFrame {
         GridBagConstraints gbc = new GridBagConstraints();
 
         MainPanel mainPanel = new MainPanel();
-        JScrollPane scrollPane = new JScrollPane(mainPanel);
+        JScrollPane scrollPane = new JScrollPane(mainPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.getVerticalScrollBar().setUnitIncrement(20);
         scrollPane.getHorizontalScrollBar().setUnitIncrement(20);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
