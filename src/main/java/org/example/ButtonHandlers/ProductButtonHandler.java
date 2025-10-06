91826a8f184b402a95f4d4fcff572226076df880
supra <alexusenzoroa@gmail.com>
2025-10-06 12:21:26 +0800

    Limited the amount of products a user can buy.
    product quantity is limited to 10, and the product can't be chosen more
    than once.

--- src/main/java/org/example/ButtonHandlers/ProductButtonHandler.java
+++ src/main/java/org/example/ButtonHandlers/ProductButtonHandler.java
@@ -10,6 +10,10 @@ import java.awt.event.*;
 public record ProductButtonHandler(Product product) implements ActionListener {
     public void actionPerformed(ActionEvent actionEvent) {
         BoughtItem boughtItem = new BoughtItem(product);
+         if (alreadyContains(product)) {
+            JOptionPane.showMessageDialog(null, "You've already chosen that product!");
+            return;
+         }
         while (true) {
             try {
                 String input = JOptionPane.showInputDialog("Input quantity of product: ");
@@ -17,6 +21,8 @@ public record ProductButtonHandler(Product product) implements ActionListener {
                     break;
                 } else if (Integer.parseInt(input) <= 0) {
                     JOptionPane.showMessageDialog(null, "Quantity must be greater than 0!");
+                } else if (Integer.parseInt(input) > 10) {
+                    JOptionPane.showMessageDialog(null, "Quantity must be lesser than 10!");
                 } else {
                     boughtItem.setQuantity(Integer.parseInt(input));
                     Order.appendOrders(boughtItem);
@@ -30,5 +36,13 @@ public record ProductButtonHandler(Product product) implements ActionListener {
                 }
             } catch (NumberFormatException e) { JOptionPane.showMessageDialog(null, "Please input a valid quantity!"); }
         }
+    }
+    private boolean alreadyContains(Product product) {
+        for (BoughtItem boughtItem : Order.getOrders()) {
+            if (boughtItem.getProduct() == product) {
+                return true;
+            }
+        }
+        return false;
     }
 }
