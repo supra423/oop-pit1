8c0b6916a47021d7d4c907ad74ffe75ccd795d81
supra <alexusenzoroa@gmail.com>
2025-10-11 20:07:27 +0800

    Added photo icon in sales inventory window

--- src/main/java/org/example/gui/SalesInventoryWindow/SalesInventoryWindow.java
+++ src/main/java/org/example/gui/SalesInventoryWindow/SalesInventoryWindow.java
@@ -1,11 +1,16 @@
 package org.example.gui.SalesInventoryWindow;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class SalesInventoryWindow extends JFrame {
     public SalesInventoryWindow() {
         this.setTitle("JNJS SALES INVENTORY");
         this.setVisible(true);
+        ImageIcon logo = new ImageIcon(Objects.requireNonNull(SalesInventoryWindow.class.getResource("/assets/logoEdited.png")));
+        this.setIconImage(logo.getImage());
+
         SalesInventoryPanel salesInventoryPanel = new SalesInventoryPanel();
 
         JScrollPane scrollPane = new JScrollPane(salesInventoryPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
